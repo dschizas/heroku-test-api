cd551c8b05289d84decfd5b45b06aba04312774e
Dimitris Schizas <dimitrisschizas@admins-MacBook-Pro-5.local>
2017-01-23 14:35:08 +0000

    perform a soap request

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // Remove two lines below for deploying to heroku
 var host = "127.0.0.1";
-var port = 9000;
+var port = 9002;
 
 var express = require("express");
 var path = require("path");
@@ -8,10 +8,19 @@ var bodyParser = require("body-parser");
 
 var app = express();
 app.use(bodyParser.json());
-// Remove two lines below for deploying to heroku
+// Remove three lines below for deploying to heroku
 app.use('/', express.static(__dirname + '/'));
 console.log('Running server at http://localhost:' + port + '/');
+// var request = require('request');
+var soap = require('soap');
+var parseString = require('xml2js').parseString;
 
+var crifCred = require('./credentials/crif_credentials.json');
+
+var url = 'http://webservices.eurotaxglass.com/wsdl/identification-v2.wsdl'; // Our WSDL
+var OrderRequestElement =   {
+	"test": "test",
+}
 
 function handleError(res, reason, message, code) {
   console.log("ERROR: " + reason);
@@ -22,6 +31,38 @@ function handleError(res, reason, message, code) {
  *    GET: test search query
  *    POST: creates a new motor record
  */
+
+app.get("/soap", function(req, res) {
+	console.log("SOAP request");
+	soap.createClient(url, function(err, client){
+		var soapHeader = {
+  			"ETGHeader": {
+  				"VersionRequest" : crifCred.versionRequest,
+  				"Originator": {
+  					"Signature" : crifCred.signature,
+  					"LoginData" : {
+	  					"Name": crifCred.name,
+	  					"Password" : crifCred.password,
+	  					"ReturnSecurityToken" : crifCred.returnSecurityToken
+	  				},
+  					"SecurityToken" : crifCred.securityToken
+  				}
+  			}
+		};
+		client.addSoapHeader(soapHeader);
+		// The Client now has all the methods of the WSDL. Use it to create a new order by feeding it the JSON Payload
+		client.GetListMake(OrderRequestElement, function(err, result, body) {
+			parseString(body, function(err, result){
+				// Get The Result From The Soap API and Parse it to JSON
+				if (err){
+					return res.status(500).json(err);
+				}
+				return res.json(result); 
+			})
+		});
+	});
+	// res.send("OK");
+});
 
 app.get("/search", function(req, res) {
     var registrationId = req.query.reg;
